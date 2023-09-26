adb42cb493a3e37398016f4afb1a2d658347cddd
sujalshrestha <suj9763@gmail.com>
2023-09-26 17:52:02 +0545

    Pulled from dev

---
+++ Discovero/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Discovero
+//
+//  Created by Sujal Shrestha on 14/09/2023.
+//
+
+import UIKit
+import FirebaseCore
+import FirebaseFirestore
+
+class ViewController: UIViewController {
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
